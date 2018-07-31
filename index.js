e6d6f1ebc145b787b1a81948a97170a953da38fc
qianjin <qianjin@learnta.com>
2018-07-31 11:51:32 +0800

    update

--- index.js
+++ index.js
@@ -13,21 +13,22 @@ const rootDir = process.cwd()
 const distDir = path.join(rootDir, eval(dist))
 
 function fileRecursion(dir = distDir) {
+    if (ignoreCheck(ignore, dir)) return ylog.info('忽略文件夹', dir)
     fs.readdir(dir, (err, files) => {
         if (err) ylog.error(err)
         else {
             files.forEach((filename) => {
-                const file = path.join(distDir, filename)
-                if (ignoreCheck(ignore, file)) {
-                    ylog.info(`忽略文件  ${file}`)
-                    return
-                }
+                const file = path.join(dir, filename)
                 fs.stat(file, (err, status) => {
                     if (err) ylog.error('获取文件status失败')
 
                     if (status.isDirectory()) {
                         fileRecursion(file)
                     } else if (status.isFile()) {
+                        if (ignoreCheck(ignore, file)) {
+                            ylog.info(`忽略文件  ${file}`)
+                            return
+                        }
                         checkIfExist(filename, prefix, uploadFile.bind(this, file, filename, prefix))
                     }
                 })
@@ -38,12 +39,8 @@ function fileRecursion(dir = distDir) {
 
 function ignoreCheck(checks, file) {
     return checks.some(c => {
-        const check = typeof c === 'string' ? eval(`/${c}/g`) : c
-        try {
-            return check.test(file)
-        } catch (e) {
-            ylog.error(e)
-        }
+        const path = rootDir + eval(dist) + c
+        return file.indexOf(path) === 0
     })
 }
 
